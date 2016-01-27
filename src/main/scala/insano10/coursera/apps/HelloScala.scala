0e1ecfd80011eda03bcb6e6fd9fe616e7fd04a2e
insano10 <jdommett@gmail.com>
2016-01-27 17:20:59 +0000

    week3, HelloScala standalone app

---
+++ src/main/scala/insano10/coursera/apps/HelloScala.scala
@@ -0,0 +1,7 @@
+package insano10.coursera.apps
+
+//a standalone application must start from a singleton object containing a main method
+object HelloScala {
+
+  def main(args: Array[String]) = println("Hello World")
+}
