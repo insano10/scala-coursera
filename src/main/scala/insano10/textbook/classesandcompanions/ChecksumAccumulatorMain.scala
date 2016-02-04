d105bc0173296aa331d30525916cf5cce09cdc9e
insano10 <jdommett@gmail.com>
2016-02-04 11:21:10 +0000

    example of a class and its companion object

---
+++ src/main/scala/insano10/textbook/classesandcompanions/ChecksumAccumulatorMain.scala
@@ -0,0 +1,9 @@
+package insano10.textbook.classesandcompanions
+
+object ChecksumAccumulatorMain {
+
+  def main(args: Array[String]): Unit = {
+
+    println(ChecksumAccumulator.calculate("hello world"))
+  }
+}
