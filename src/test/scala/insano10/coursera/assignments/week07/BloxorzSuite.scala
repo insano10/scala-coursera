3e485434501db9e2b3becda0b0e7f9bde4d85e1f
insano10 <jdommett@gmail.com>
2016-02-08 16:27:02 +0000

    week7, assignment, bloxorz, implement newNeighboursOnly with a test

--- src/test/scala/insano10/coursera/assignments/week07/BloxorzSuite.scala
+++ src/test/scala/insano10/coursera/assignments/week07/BloxorzSuite.scala
@@ -69,6 +69,20 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("newNeighboursOnly level 1") {
+    new Level1 {
+
+      val neighbours: Stream[(Block, List[Move])] = Set(
+        (Block(Pos(1, 2), Pos(1, 3)), List(Right, Left, Up)),
+        (Block(Pos(2, 1), Pos(3, 1)), List(Down, Left, Up))
+      ).toStream
+
+      val explored: Set[Block] = Set(Block(Pos(1, 2), Pos(1, 3)), Block(Pos(1, 1), Pos(1, 1)))
+
+      assert(newNeighborsOnly(neighbours, explored) == Set((Block(Pos(2, 1), Pos(3, 1)), List(Down, Left, Up))).toStream)
+    }
+  }
+
 
   test("optimal solution for level 1") {
     new Level1 {
