1d027ddc77fb3e5acd3f56cd7915fd4e7d7ea233
insano10 <jdommett@gmail.com>
2016-02-08 15:36:01 +0000

    week7, assignment, bloxorz, implement findChar

--- src/main/scala/insano10/coursera/assignments/week07/streams/StringParserTerrain.scala
+++ src/main/scala/insano10/coursera/assignments/week07/streams/StringParserTerrain.scala
@@ -65,7 +65,11 @@ trait StringParserTerrain extends GameDef {
     * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
     * `Vector` class
     */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+
+    val rowIdx = levelVector.indexWhere(row => row.indexOf(c) >= 0)
+    Pos(rowIdx, levelVector(rowIdx).indexOf(c))
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
